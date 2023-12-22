c7fa4a4dc27712f85f490c4062ebe05186aad023
youssufeltahan <117551034+youssufeltahan@users.noreply.github.com>
2023-12-22 21:03:10 +0200

    Update settings.py

--- toolsproject/settings.py
+++ toolsproject/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+os.environ
+#port = os.environ['port']
+#db_port = os.environ['db_port']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
